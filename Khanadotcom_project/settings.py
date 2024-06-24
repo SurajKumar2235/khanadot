07ffd9b630d4b655693443c531ecb2380afdddda
Ashura <ashuramajestic@gmail.com>
2024-06-24 11:07:06 +0530

    ashura

--- Khanadotcom_project/settings.py
+++ Khanadotcom_project/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,14 +30,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
 # email sender
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True  # TLS (Transport Layer Security) protocol is used
-EMAIL_HOST_USER = "ashuramajestic@gmail.com"
-EMAIL_FROM = "ashuramajestic@gmail.com"
-EMAIL_HOST_PASSWORD = "djqynlkuarwwzezl"  # Your Gmail password or App Password
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_FROM = os.getenv("EMAIL_FROM")  # Your Gmail password or App Password
 PASSWORD_RESET_TIMEOUT = 14400
 
 
