ccc081b85ec1a952dbd0e9ae91ec980d470bc55a
Ashura <ashuramajestic@gmail.com>
2024-07-03 19:32:09 +0530

    api testing

--- Khanadotcom_project/settings.py
+++ Khanadotcom_project/settings.py
@@ -95,11 +95,11 @@ WSGI_APPLICATION = "khanadotcom_project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "khanadotcomdb",
-        "USER": "khana",
-        "PASSWORD": "Admin@123",
-        "HOST": "khanadotcom.mysql.database.azure.com",
-        "PORT": "3306",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT"),
     },
 }
 
