aa3edec2787f13aef42e8f2de668ba8b751b27fd
Ashura <ashuramajestic@gmail.com>
2024-07-09 13:21:53 +0530

    increased activation emil time

--- khanadotcom_app/tokens.py
+++ khanadotcom_app/tokens.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 import six
+from datetime import timedelta
 
 
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
             six.text_type(user.pk)
-            + six.text_type(timestamp)
+            + six.text_type(timestamp + timedelta(hours=12).total_seconds())
             + six.text_type(user.is_active)
         )
 
